58106edd8f608147f63e51a29c35769306f8aba3
evidyasagar <42410939+evidyasagar@users.noreply.github.com>
2020-12-04 17:49:14 +0530

    code updated

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,7 @@
 # set the base image
 FROM openjdk:8
 
-#add resources to the container
-VOLUME /tmp
-
 EXPOSE 8080
-# add scripts to the container
-ADD target/helloworld.jar helloworld.jar
+ADD target/hellospring.jar hellospring
 
-# install app runtimes and modules
-RUN bash -c 'touch /helloworldspring.jar'
-RUN apt-get update
-
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom", "-jar", "/helloworldspring.jar"]
+ENTRYPOINT ["java","-jar", "/hellospring"]
--- src/main/java/com/example/helloworldspringboot/HelloworldSpringbootApplication.java
+++ src/main/java/com/example/helloworldspringboot/HelloworldSpringbootApplication.java
@@ -1,18 +1,19 @@
 package com.example.helloworldspringboot;
 
-import jdk.nashorn.internal.runtime.logging.Logger;
-import org.slf4j.LoggerFactory;
+/*import jdk.nashorn.internal.runtime.logging.Logger;*/
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Component
 /**
  * below are the annotations used to call appropriate packages, classes and its relevant methods
  */
 @RestController
 @SpringBootApplication
-@Logger
+// @Logger
 public class HelloworldSpringbootApplication
 {
 	//private static final Logger LOGGER=LoggerFactory.getLogger(HelloworldSpringbootApplication.class);
